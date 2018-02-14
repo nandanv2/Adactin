7103e43044034748602c5912b5377395bce804f7
admin <admin@192.168.1.5>
2018-02-14 10:48:31 +0530

    my first commit on feb 14

---
+++ TestNGProject/src/comTestNGProject/Validationsw.java
@@ -0,0 +1,28 @@
+package comTestNGProject;
+
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Validationsw {
+	
+	@Test
+	public void Test1() {
+		
+	String a= "Antony George";
+    String b="Antony George";
+    Assert.assertEquals(a,b);
+      
+	}
+	
+	@Test
+	public void Test2() {
+		String a= "Antony George";
+	    String b="Antony GEORGE";
+	    Assert.assertEquals(a,b);
+}
+} 
+	
+ 
+
+
